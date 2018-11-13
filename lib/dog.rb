94924e4ed8988b949605dff4c8da36d7f17ffc06
Brandon Teich <brandonteich123@gmail.com>
2018-11-13 23:53:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
 class Dog 
-  def name= dog
-    @name = dog
+  def name= name 
+    @name = name
   end 
-end
+  
+  def name 
+    @name
+  end 
+  
+  def breed= breed 
+    @breed = breed 
+  end 
+  
+  def breed 
+    @breed 
+  end 
+end  
