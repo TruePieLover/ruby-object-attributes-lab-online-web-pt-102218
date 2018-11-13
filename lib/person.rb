64b606de22035e8e3f354371c47c7a2140058511
Brandon Teich <brandonteich123@gmail.com>
2018-11-13 23:54:31 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,15 +2,12 @@ class Person
   def name=(personname)
     @name = personname
   end 
-  
   def name 
     @name 
   end 
-  
   def job=(jobtype)
     @job = jobtype
   end 
-  
   def job 
     @job 
   end 
